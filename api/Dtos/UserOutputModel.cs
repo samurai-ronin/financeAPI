53346e8ecb094cc73aac97c8f3be26c77e5c39b6
Mauro Tarquino <rucavalente@gmail.com>
2021-12-16 10:00:55 +0100

    feat:adicionando lista de contas

--- api/Dtos/UserOutputModel.cs
+++ api/Dtos/UserOutputModel.cs
@@ -1,8 +1,11 @@
+using System.Collections.ObjectModel;
+
 namespace api.Dtos
 {
     public class UserOutputModel
     {
         public int id { get; set; }
         public string name { get; set; }
+        public Collection<AccountOutputModel> accounts { get; set; }
     }
 }
